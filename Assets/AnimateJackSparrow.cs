8c1b7b4551750b35f868ec1123e2ee783b9eeeb7
Abel <Abel.Beek@student.uib.no>
2022-04-21 13:59:26 +0200

    Initial commit

---
+++ Assets/AnimateJackSparrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateJackSparrow : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WalkForwardStart");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetTrigger("WalkForwardStop");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("WalkBackwardStart");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetTrigger("WalkBackwardStop");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Assets/ControlJackSparrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJackSparrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
